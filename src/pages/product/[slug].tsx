41215467e9105bcca8848757ef07f80772bb1abe
boomchanotai <boom9387@hotmail.com>
2023-07-15 17:52:20 +0700

    feat: firebase get

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,13 +1,57 @@
 import MainLayout from "@/layouts/MainLayout";
-import Product from "@/components/Home/Product";
 import Container from "@/components/Container";
 
 import Link from "next/link";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import app from "@/utils/firebase";
+import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
-const ProductPage = () => {
+const ProductPage = ({ query }: { query: string }) => {
+  const db = getFirestore(app);
+  const router = useRouter();
+
+  const [product, setProduct] = useState<any>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (router.query.slug) {
+        const docRef = doc(db, "products", query);
+        const docSnap = await getDoc(docRef);
+        const data = docSnap.data();
+        setProduct(data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(product);
+  }, [product]);
+
   return (
     <MainLayout>
-      <Product />
+      <Container className="py-16">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
+          <div>
+            <div className="relative w-full aspect-video">
+              <Image
+                src={product.image}
+                fill={true}
+                className="object-cover"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="space-y-4">
+            <h2 className="font-bold text-2xl">{product.name}</h2>
+            <p>{product.description}</p>
+          </div>
+        </div>
+      </Container>
       <Container className="space-y-6">
         <div>
           <label
@@ -40,6 +84,12 @@ const ProductPage = () => {
       </Container>
     </MainLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<{
+  query: string | string[] | undefined;
+}> = async ({ query }) => {
+  return { props: { query: query.slug } };
 };
 
 export default ProductPage;
