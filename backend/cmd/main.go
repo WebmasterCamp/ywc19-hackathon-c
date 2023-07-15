485bc43efba6e00c5a36a333b605134b42af3ef4
Parinya Termkasipanich <40797988+parinyapt@users.noreply.github.com>
2023-07-16 04:38:28 +0700

    add init route

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/WebmasterCamp/ywc19-hackathon-c/backend/config"
 	"github.com/WebmasterCamp/ywc19-hackathon-c/backend/database"
 	"github.com/WebmasterCamp/ywc19-hackathon-c/backend/logger"
+	"github.com/WebmasterCamp/ywc19-hackathon-c/backend/routes"
 )
 
 func main() {
@@ -14,4 +15,5 @@ func main() {
 	logger.InitializeLogger(os.Getenv("DEPLOY_MODE"))
 	config.InitializeConfig()
 	database.InitializeDatabase()
+	routes.InitializeRoutes()
 }
