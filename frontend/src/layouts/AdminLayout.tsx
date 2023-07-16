5ba28dbc4af40eaed37a0f08c82ff8c030950e5f
boomchanotai <boom9387@hotmail.com>
2023-07-16 12:13:42 +0700

    feat: lgoo

--- frontend/src/layouts/AdminLayout.tsx
+++ frontend/src/layouts/AdminLayout.tsx
@@ -100,12 +100,8 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
               </Transition.Child>
               <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
                 <div className="flex-shrink-0 flex items-center px-4">
-                  <div className="relative h-8 aspect-video">
-                    <Image
-                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                      alt="Workflow"
-                      fill={true}
-                    />
+                  <div className="relative w-24 aspect-video">
+                    <Image src="/logo.svg" alt="Workflow" fill={true} />
                   </div>
                 </div>
                 <nav className="mt-5 px-2 space-y-1">
@@ -173,12 +169,8 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
           <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-white">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
               <div className="flex items-center flex-shrink-0 px-4">
-                <div className="relative h-8 aspect-video">
-                  <Image
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                    alt="Workflow"
-                    fill={true}
-                  />
+                <div className="relative w-24 aspect-video">
+                  <Image src="/logo.svg" alt="Workflow" fill={true} />
                 </div>
               </div>
               <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
