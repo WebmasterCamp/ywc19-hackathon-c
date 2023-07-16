1ae4911bfdbd60004d2c91932d885d9870aa1cf2
boomchanotai <boom9387@hotmail.com>
2023-07-16 07:24:34 +0700

    feat: improve dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,10 @@
+import { getBills } from "@/api/admin/bills";
 import Container from "@/components/Container";
 import Stats from "@/components/Dashboard/Stats";
 import Table from "@/components/Dashboard/Table";
 import AdminLayout from "@/layouts/AdminLayout";
+import { TBills, TBillsResponse } from "@/types/bills";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const stats = [
@@ -28,26 +31,16 @@ const Dashboard = () => {
     },
   ];
 
-  const people = [
-    {
-      name: "Jane Cooper",
-      title: "Regional Paradigm Technician",
-      role: "Admin",
-      email: "jane.cooper@example.com",
-    },
-    {
-      name: "Jane Cooper",
-      title: "Regional Paradigm Technician",
-      role: "Admin",
-      email: "jane.cooper@example.com",
-    },
-    {
-      name: "Jane Cooper",
-      title: "Regional Paradigm Technician",
-      role: "Admin",
-      email: "jane.cooper@example.com",
-    },
-  ];
+  const [bills, setBills] = useState<TBills[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data }: TBillsResponse = await getBills();
+      setBills(data);
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <AdminLayout>
@@ -55,28 +48,27 @@ const Dashboard = () => {
         <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
         <Stats stats={stats} />
         <h2 className="font-bold">Latest Bills</h2>
-        <Table tableHead={["Name", "Title", "Email", "Role"]}>
-          {people.map((person) => (
-            <tr key={person.email}>
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                {person.name}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {person.title}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {person.email}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {person.role}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                  Edit
-                </a>
-              </td>
-            </tr>
-          ))}
+        <Table tableHead={["ID", "CreatedAt", "User", "Products"]}>
+          {bills.length != 0 &&
+            bills.map((bill: TBills, index: number) => (
+              <tr key={index}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {index + 1}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {new Date(bill.createdAt.seconds * 1000).toLocaleString()}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {bill.users.username}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {bill.products.map((product: any, index: number) => (
+                    <div key={index}>{product.name}</div>
+                  ))}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-4"></td>
+              </tr>
+            ))}
         </Table>
       </Container>
     </AdminLayout>
